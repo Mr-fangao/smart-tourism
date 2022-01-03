744e064681f746be679f77d1bc2cfb23067a2bc5
Mr-fangao <Mr.undefine@protonmail.com>
2022-01-03 21:01:20 +0800

    跨域设置

--- config/index.js
+++ config/index.js
@@ -10,11 +10,20 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8098/', //你要跨域的网址 比如 'http://news.baidu.com',
+        // secure: true, // 如果是https接口，需要配置这个参数
+        changeOrigin: true,
+        pathRewrite: {
+        '^/api': ''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8099, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
