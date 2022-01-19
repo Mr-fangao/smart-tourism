4f3ed2bf0fb593051854408eddecb9d7dc930416
gxzan <2465988596@qq.com>
2022-01-19 21:05:10 +0800

    登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,113 +1,148 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
+  <div class="login">
+    <div class="header">
+      
+      <h1>就业推荐与智能分析系统</h1>
+    </div>
+    <div class="main">
+      <div class="login_style">
+        <h2>用户平台登录</h2>
+        <el-form
+          ref="loginFrom"
+          :model="loginFrom"
+          class="login-from"
+          auto-complete="on"
         >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+          <el-form-item>
+            <img src="../assets/login/user.png" />
+            <el-input
+              class="username"
+              placeholder="请输入用户账号"
+              v-model="loginFrom.username"
+              type="text"
+              clearable
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <img src="../assets/login/password.png" />
+            <el-input
+              placeholder="请输入用户密码"
+              class="password"
+              v-model="loginFrom.password"
+              show-password
+              type="password"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox class="check">记住登录密码</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button id="btn" class="button" type="primary" round @click="handleLogin"
+              >立即登录</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Login',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      loginFrom: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  mounted() {
+    //登录绑定事件
+    window.addEventListener("keydown", this.keyDown);
+  },
+  methods: {
+    keyDown(e) {
+      //如果是回车则执行登录方法
+      if (e.keyCode == 13) {
+        document.getElementById("btn").click();
+      }
+    },
+      },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="less" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background: url(../assets/login/login_background.jpg);
+  background-size: 100% 100%;
+  .header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 10%;
+    width: 100%;
+   
+    h1 {
+      font-size: 24px;
+      color: #ffffff;
+      font-family: Microsoft YaHei;
+      font-weight: bold;
+      margin-left: 20px;
+    }
+  }
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-size: 100% 100%;
+    margin: 150px 0px;
+    .login_style {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      position: relative;
+      width: 40%;
+      height: 70%;
+      .login-from {
+        display: flex;
+        flex-direction: column;
+      }
+      h2 {
+        font-size: 20px;
+        color: #ffffff;
+        font-family: Microsoft YaHei;
+        font-weight: bold;
+        margin-bottom: 20px;
+      }
+      img {
+        height: 20px;
+        margin-top: 10px;
+      }
+      .username {
+        border-bottom: 1px solid #b8b6b6;
+        width: 250px;
+      }
+      .password {
+        border-bottom: 1px solid #b8b6b6;
+        width: 250px;
+      }
+      .check {
+        color: #ffffff;
+        margin-left: 5px;
+      }
+      .button {
+        width: 250px;
+        margin-left: 15px;
+      }
+    }
+  }
 }
 </style>
