cd223da54f078a387356ec5d60db9bbd9084d32d
Mr-fangao <Mr.undefine@protonmail.com>
2022-01-19 16:19:41 +0800

    引入elementUi
    1

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <!-- element组件测试 -->
+    <el-button type="primary" icon="el-icon-edit"></el-button>
+    <el-button type="primary" icon="el-icon-share"></el-button>
+    <el-button type="primary" icon="el-icon-delete"></el-button>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary"
+      >上传<i class="el-icon-upload el-icon--right"></i
+    ></el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'recommend',
-  data () {
+  name: "recommend",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
