fa71ff90fc3630fce611593d8f33806bac92c26c
gxzan <2465988596@qq.com>
2022-01-21 23:09:59 +0800

    标题栏样式及路由

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,17 @@
 <template>
   <nav>
+    <div class="name">
+      <img src="../assets/img/logo.png" />
+      <h1>行游天下·景点智能分析与推荐系统</h1>
+    </div>
     <ul>
       <li>
-        <router-link to="/1" exact>主页</router-link>
-        <router-link to="/2" exact>时空分析</router-link>
-        <router-link to="/thememap" exact>专题地图</router-link>
+        <router-link to="/recommend" exact>智能推荐</router-link>
+        <router-link to="/monitor" exact>景点监测</router-link>
+        <router-link to="/query" exact>景点查询</router-link>
+        <router-link to="/skAnalysis" exact>综合分析</router-link>
         <router-link to="/feelings" exact>情感分析</router-link>
+        <router-link to="/thememap" exact>专题地图</router-link>
       </li>
     </ul>
     <span class="times">{{ time }}</span>
@@ -48,34 +54,64 @@ export default {
 </script>
 
 <style scoped lang="less">
-ul {
-  list-style-type: none; /*消除黑点*/
-  text-align: center;
-  margin: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #fff; /*设置字体颜色*/
-  text-decoration: none; /*消除下划线*/
-  padding: 12px;
-  border-radius: 5px;
-}
 nav {
-  .times {
-    margin-right: 20px;
-    float: right;
-  }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   height: 50px;
-  background: grey;
-  padding: 0px 0;
+  background: url(../assets/img/header.png);
+  background-size: 100% 100%;
+  padding: 0px;
+  .name {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    img {
+      height: 32px;
+      padding-left: 20px;
+    }
+    h1 {
+      font-size: 30px;
+      color: #000000;
+      font-family: Microsoft YaHei;
+      font-weight: bold;
+      margin-left: 10px;
+    }
+  }
+  .times {
+    margin-right: 10px;
+    float: right;
+    font-family: Microsoft YaHei;
+    font-weight: bold;
+  }
+  ul {
+    height: 100%;
+    list-style-type: none; /*消除黑点*/
+    text-align: center;
+    margin: 0px 0px 0px 60px;
+    li {
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin: 0 20px;
+      a {
+        font-size: 20px;
+        color: #000000; /*设置字体颜色*/
+        font-family: Microsoft YaHei;
+        font-weight: bold;
+        text-decoration: none; /*消除下划线*/
+        border-radius: 5px;
+        margin: 0px 12px 0px 0px;
+        padding: 10px;
+        background: url(../assets/img/background.png);
+        background-size: 100% 100%;
+      }
+    }
+  }
 }
 /*选中则发生一些变化*/
 .router-link-active {
-  background: rgba(255, 255, 255, 0.8);
-  color: #444;
+  color: #36a503;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,48 +11,64 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-      path: '/',
-      name: 'Login',
-      component: Login,
-      meta: {
-        keepAlive: false //导航栏在该页面不显示
-      }
-    },
-    {
-      path: '/1',
-      name: 'recommend',
-      component: recommend,
-      meta: {
-        keepAlive: true
-      }
-    },
-    {
-      path: '/2',
-      name: 'skAnalysis',
-      component: skAnalysis,
-      meta: {
-        keepAlive: true
-      }
-    },
-    {
-      path: '/thememap',
-      name: 'thememap',
-      component: thememap,
-      meta: {
-        keepAlive: true
-      }
-    },
-    {
-      path: '/feelings',
-      name: 'feelings',
-      component: feelings,
-      meta: {
-        keepAlive: true
-      }
-    },
-    {
-      path: "*", // 此处需特别注意置于最底部
-      component: notFound
+    path: '/',
+    name: 'Login',
+    component: Login,
+    meta: {
+      keepAlive: false //导航栏在该页面不显示
     }
+  },
+  {
+    path: '/recommend',
+    name: 'recommend',
+    component: () => import("@/views/recommend"),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '/monitor',
+    name: 'monitor',
+    component: () => import("@/views/monitor"),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '/query',
+    name: 'query',
+    component: () => import("@/views/query"),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '/skAnalysis',
+    name: 'skAnalysis',
+    component: () => import("@/views/skAnalysis"),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '/feelings',
+    name: 'feelings',
+    component: () => import("@/views/feelings"),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '/thememap',
+    name: 'thememap',
+    component: () => import("@/views/theme"),
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: "*", // 此处需特别注意置于最底部
+    component: notFound
+  },
   ]
 })
