2ba5ab5ce2b475e7cfa371af1574e57c4d8f8fda
Mr-fangao <Mr.undefine@protonmail.com>
2022-01-21 17:51:29 +0800

    404页面
    1

--- src/router/index.js
+++ src/router/index.js
@@ -5,49 +5,54 @@ import recommend from '@/views/recommend'
 import skAnalysis from '@/views/skAnalysis'
 import thememap from '@/views/theme'
 import feelings from '@/views/feelings'
+import notFound from '../components/notFound.vue'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Login',
       component: Login,
-      meta:{
-        keepAlive:false//导航栏在该页面不显示
+      meta: {
+        keepAlive: false //导航栏在该页面不显示
       }
     },
     {
       path: '/1',
       name: 'recommend',
       component: recommend,
-      meta:{
-        keepAlive:true
+      meta: {
+        keepAlive: true
       }
     },
     {
       path: '/2',
       name: 'skAnalysis',
       component: skAnalysis,
-      meta:{
-        keepAlive:true
+      meta: {
+        keepAlive: true
       }
     },
     {
       path: '/thememap',
       name: 'thememap',
       component: thememap,
-      meta:{
-        keepAlive:true
+      meta: {
+        keepAlive: true
       }
     },
-        {
+    {
       path: '/feelings',
       name: 'feelings',
       component: feelings,
-      meta:{
-        keepAlive:true
+      meta: {
+        keepAlive: true
       }
     },
+    {
+      path: "*", // 此处需特别注意置于最底部
+      component: notFound
+    }
   ]
 })
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  timeout: 5000,
+  timeout: 8000,
 });
 
 // request 拦截器
@@ -11,24 +11,19 @@ request.interceptors.request.use(
   (config) => {
     config.headers["Content-Type"] = "application/json;charset=utf-8";
 
-    // config.headers['token'] = user.token;  // 设置请求头
+
     return config;
   },
   (error) => {
     return Promise.reject(error);
   }
 );
-
-// response 拦截器
-// 可以在接口响应后统一处理结果
 request.interceptors.response.use(
   (response) => {
     let res = response.data;
-    // 如果是返回的文件
     if (response.config.responseType === "blob") {
       return res;
     }
-    // 兼容服务端返回的字符串数据
     if (typeof res === "string") {
       res = res ? JSON.parse(res) : res;
     }
