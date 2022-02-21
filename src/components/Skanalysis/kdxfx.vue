436b5b23f7606ade982bc37ef0c80dd09590c2b0
“chenjq001” <2862053319@qq.com>
2022-02-21 17:45:35 +0800

    按钮

--- src/components/Skanalysis/kdxfx.vue
+++ src/components/Skanalysis/kdxfx.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="com-kdxfx">
-    2
-    <div id="map"></div>
+    <div id="map">
+      <div class="page">
+        <div class="top">
+            <el-button>上左</el-button>
+            <el-button>上边</el-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "kdxfx",
-  mounted(){
+  mounted() {
     this.initmap();
   },
   methods: {
@@ -21,18 +27,36 @@ export default {
         zoom: 4,
       });
     },
-  }, 
+  },
 };
 </script>
 
 <style scoped lang="less">
 .com-kdxfx {
   #map {
-     position: absolute;
-     width: 100%; 
-     height: 100%;
-  }
-  //background: rgb(29, 31, 163);
+    position: absolute;
+    width: 100%;
+    height: 100%;
 
+    .page {
+      position: absolute; /*这里一定要设置*/
+      z-index: 999999; /*这里是该元素与显示屏的距离，据说越大越好，因为没有它也是可以的*/
+      float: right inherit;
+      width: 80px;
+      float: right;
+      
+        margin-right: 1px;
+      .item {
+        margin: 4px;
+      }
+      .top {
+        text-align: center;
+        margin-right: 1px;
+      }
+
+    }
+  }
+
+  //background: rgb(29, 31, 163);
 }
 </style>
