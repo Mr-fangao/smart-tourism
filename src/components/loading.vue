c5c98f59a8a9a23b3379e4e74102f760681b147d
gxzan <2465988596@qq.com>
2022-03-02 13:59:14 +0800

    加载动画

--- src/components/loading.vue
+++ src/components/loading.vue
@@ -1,19 +1,28 @@
 <template>
-<div class="loading"></div>
+  <div class="loading">
+    <div class="load-4">
+      <p>Loading</p>
+      <div class="ring-1"></div>
+    </div>
+  </div>
 </template>
 <script>
+import "../assets/css/loading.css";
 export default {
-  name: 'Loading' // 定义的组件名称 使用时写法：loading
-}
+  name: "Loading", // 定义的组件名称 使用时写法：loading
+};
 </script>
-<style scoped>
+<style  lang="less" scoped>
 .loading {
   position: fixed;
   left: 0;
   top: 0;
-  background: url('../assets/img/loading.gif') center center no-repeat;
-  width: 100vw;
-  height: 100vh;
-  z-index: 1000;
+  background-color: rgba(156, 187, 189, 0.486);
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  .load-4{
+z-index: 100;
+  }
 }
 </style>
