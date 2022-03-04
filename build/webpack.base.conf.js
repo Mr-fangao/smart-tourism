2035fa18790f4ee3a71947dbad416decafb6d74f
Mr-fangao <Mr.undefine@protonmail.com>
2022-03-04 19:00:49 +0800

    其他
    其他

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,7 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-
+var webpack = require('webpack')
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -11,6 +11,13 @@ function resolve (dir) {
 
 
 module.exports = {
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    new webpack.ProvidePlugin({
+      jQuery: "jquery",
+      $: "jquery"
+    })
+  ],
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-
+import 'jquery'
 // 引入echarts
 import * as echarts from 'echarts';
 import 'echarts-gl'
