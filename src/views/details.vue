091cff94df16871e6ded4efbe7a31f8304c812a4
Mr-fangao <Mr.undefine@protonmail.com>
2022-03-09 23:29:08 +0800

    1
    1

--- src/views/details.vue
+++ src/views/details.vue
@@ -1,12 +1,46 @@
 <template>
   <div class="details">
     <div class="details-left part">
-      <div class="content"></div>
-      <div class="content"></div>
-      <div class="content"></div>
+      <div class="content">
+        <div class="title">
+          <span>精选图片</span>
+        </div>
+        <div class="con"></div>
+      </div>
+      <div class="content">
+        <div class="title">
+          <span>游客数据</span>
+        </div>
+        <div class="con"></div>
+      </div>
+      <div class="content">
+        <div class="title">
+          <span>游客月变化图</span>
+        </div>
+        <div class="con"></div>
+      </div>
     </div>
     <div class="details-mid part"></div>
-    <div class="details-righ part"></div>
+    <div class="details-righ part">
+      <div class="content">
+        <div class="title">
+          <span>精选图片</span>
+        </div>
+        <div class="con"></div>
+      </div>
+      <div class="content">
+        <div class="title">
+          <span>游客来源分布</span>
+        </div>
+        <div class="con"></div>
+      </div>
+      <div class="content">
+        <div class="title">
+          <span>游客月变化图</span>
+        </div>
+        <div class="con"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,24 +75,80 @@ export default {
   display: flex;
   .part {
     flex: 1;
-    height: 100%;
+    height: 93%;
+    padding: 0.7%;
   }
   .part:nth-child(1) {
+    background: url(../assets/img/side.png) no-repeat;
+    opacity: 1;
+    background-size: 95% 98%;
+    background-position: 36% 40%;
     display: flex;
     flex-direction: column;
     flex-wrap: nowrap;
     .content {
       width: 100%;
-      height: 31%;
+      height: 33.3%;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: nowrap;
+      justify-content: center;
+      .title {
+        flex: 1;
+        width: 100%;
+        background: url("../assets/img/titlebg.png") no-repeat;
+        background-size: 50% 80%;
+        background-position-x: 6%;
+        > span {
+          color: rgb(178, 228, 228);
+          position: absolute;
+          left: 4%;
+          line-height: 28px;
+          font-size: 11pt;
+        }
+      }
+      .con {
+        flex: 6;
+        width: 100%;
+      }
     }
   }
   .part:nth-child(2) {
     flex: 2;
   }
   .part:nth-child(3) {
+    background: url(../assets/img/side.png) no-repeat;
+    opacity: 1;
+    background-size: 95% 98%;
+    background-position: 36% 40%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
     .content {
       width: 100%;
-      height: 31%;
+      height: 33.3%;
+      display: flex;
+      flex-direction: column;
+      flex-wrap: nowrap;
+      justify-content: center;
+      .title {
+        flex: 1;
+        width: 100%;
+        background: url("../assets/img/titlebg.png") no-repeat;
+        background-size: 50% 80%;
+        background-position-x: 6%;
+        > span {
+          color: rgb(178, 228, 228);
+          position: absolute;
+          left: 4%;
+          line-height: 28px;
+          font-size: 11pt;
+        }
+      }
+      .con {
+        flex: 6;
+        width: 100%;
+      }
     }
   }
 }
