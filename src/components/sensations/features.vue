5511ce79e7cf2cfdfa262e909adfdd9487eeee24
Mr-fangao <Mr.undefine@protonmail.com>
2022-05-03 22:55:32 +0800

    其他
    无重要内容

--- src/components/sensations/features.vue
+++ src/components/sensations/features.vue
@@ -65,100 +65,14 @@ export default {
     handleResize() {
       this.myChart2 && this.myChart2.resize();
     },
-    initmap(val) {
+    initmap() {
       this.$mapboxgl.accessToken =
         "pk.eyJ1IjoiY2hlbmpxIiwiYSI6ImNrcWFmdWt2bjBtZGsybmxjb29oYmRzZzEifQ.mnpiwx7_cBEyi8YiJiMRZg";
       var map = new this.$mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/chenjq/cl084urgf004014ny2nhu1xre",
       });
-      map.on("load", () => {
-        if (val == "北京") {
-          map.flyTo({
-            center: [118.77949013671878, 32.04501247139361],
-            // center: [116.40811432812498, 39.90388639121686],
-            zoom: 10.5, // 缩放比例
-            pitch: 45, // 倾斜度
-          });
-        }
-        // else if (val == "北京") {
-        //   map.flyTo({
-        //     center: [116.40811432812498, 39.90388639121686], // 中心点
-        //     zoom: 10.5, // 缩放比例
-        //     pitch: 45, // 倾斜度
-        //   });
-        // }
-        map.addSource("njmark", {
-          type: "geojson",
-          data: nanjing,
-        });
-        map.addSource("njpolygon", {
-          type: "geojson",
-          data: njprovince,
-        });
-        map.addLayer({
-          id: "fillID",
-          type: "fill" /* symbol类型layer，一般用来绘制点*/,
-          source: "njpolygon",
-          paint: {
-            "fill-color": "#74add1",
-            "fill-opacity": 0.6 /* 透明度 */,
-          },
-        });
-        map.addLayer({
-          id: "earthquakes-layer",
-          type: "circle",
-          source: "njmark",
-          paint: {
-            "circle-radius": 5,
-            "circle-stroke-width": 2,
-            "circle-opacity": 0.75,
-            "circle-stroke-color": "white",
-            "circle-color": [
-              "interpolate",
-              ["linear"],
-              ["get", "score"],
-              3.5,
-              "#ACBE21",
-              3.8,
-              "#BEB221",
-              4.1,
-              "#BE8221",
-              4.4,
-              "#FF8C00",
-              4.7,
-              "#BE6321",
-              5.0,
-              "#BE4521",
-            ],
-          },
-        });
-        // map.addLayer({
-        //   id: "earthquakes-layer",
-        //   type: "circle",
-        //   source: "njmark",
-        //   paint: {
-        //     "circle-radius": 5,
-        //     "circle-stroke-width": 2,
-        //     "circle-color": "#06b2fb",
-        //     "circle-stroke-color": "white",
-        //   },
-        // });
-        map.addLayer({
-          id: "points",
-          type: "symbol" /* symbol类型layer，一般用来绘制点*/,
-          source: "njmark",
-          layout: {
-            "text-field": ["get", "name"],
-            "text-offset": [1.5, 1.5],
-          },
-          paint: {
-            "text-color": "#FFFFFF",
-          },
-        });
-        const countyLegendEl = document.getElementById("county-legend");
-      });
-    },
+    }
   },
   watch: {},
 };
